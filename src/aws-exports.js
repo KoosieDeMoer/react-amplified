04f4060e9ed2c58637432b476d187aaac4524f7f
KoosieDeMoer <arseabout@gmail.com>
2020-06-10 09:11:17 +0200

    Fix aws_user_pools_id

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,7 @@ const awsmobile = {
     "aws_appsync_apiKey": "da2-rdb6cpaxprcevo7b6aqfwmarom",
     "aws_cognito_identity_pool_id": "us-east-1:5ff6b5ff-adac-4d32-bcd8-6568c5292e10",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "appreactamplifyc3f5219d_userpool_c3f5219d-dev",
+    "aws_user_pools_id": "us-east-1_pm5nC5yW6",
     "aws_user_pools_web_client_id": "28bd7qsan1kh1l8ep7ko1g0p0p",
     "oauth": {}
 };
